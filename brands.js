77059e3b920b11c225fd10a342218a3185bcaee3
hatimkd <hatimkd7192@gmail.com>
2025-03-19 06:15:17 -0700

    Updates

---
+++ brands.js
@@ -0,0 +1,63 @@
+
+const brands = [
+    { value: "BMW", label: "BMW", models: ["Serie 3", "Serie 5", "X5"] },
+    {
+      value: "Audi",
+      label: "Audi",
+      models: ["A3", "A4", "Q5", "Q8 e-tron", "A7"],
+    },
+    {
+      value: "Dacia",
+      label: "Dacia",
+      models: ["Logan", "Sandero Stepway", "Sandero"],
+    },
+    {
+      value: "Mercedes-Benz",
+      label: "Mercedes-Benz",
+      models: ["Classe A", "Classe C", "E-Class", "GLE"],
+    },
+    {
+      value: "Volkswagen",
+      label: "Volkswagen",
+      models: ["Golf", "Passat", "Tiguan", "Touareg"],
+    },
+    { value: "Porsche", label: "Porsche", models: ["911", "Cayenne", "Macan"] },
+    { value: "Hyundai", label: "Hyundai", models: ["Accent", "Tucson"] },
+
+    // Marques populaires en France
+    {
+      value: "Renault",
+      label: "Renault",
+      models: ["Clio", "Captur", "Mégane"],
+    },
+    { value: "Peugeot", label: "Peugeot", models: ["208", "3008", "508"] },
+    { value: "Citroën", label: "Citroën", models: ["C3", "C4", "C5 Aircross"] },
+    {
+      value: "DS Automobiles",
+      label: "DS Automobiles",
+      models: ["DS3", "DS4", "DS7"],
+    },
+    { value: "Alpine", label: "Alpine", models: ["A110"] },
+
+    // Marques populaires en Italie
+    { value: "Fiat", label: "Fiat", models: ["500", "Panda", "Tipo"] },
+    {
+      value: "Alfa Romeo",
+      label: "Alfa Romeo",
+      models: ["Giulia", "Stelvio", "Mito"],
+    },
+    { value: "Lancia", label: "Lancia", models: ["Ypsilon", "Delta"] },
+    {
+      value: "Ferrari",
+      label: "Ferrari",
+      models: ["488", "Portofino", "Roma"],
+    },
+    {
+      value: "Maserati",
+      label: "Maserati",
+      models: ["Ghibli", "Levante", "Quattroporte"],
+    },
+    { value: "Toyota", label: "Toyota", models: ["Corolla", "Camry", "Hilux"] },
+    { value: "Honda", label: "Honda", models: ["Civic", "Accord", "CR-V"] },
+    { value: "Ford", label: "Ford", models: ["Focus", "Mustang", "F-150"] },
+  ];
